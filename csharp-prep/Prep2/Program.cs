94f27a8793ddde5c2a9e94b4f2c859ab09271c3c
Jared <Kou2001@byui.edu>
2024-05-01 13:00:02 -0600

    finish the homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         Console.Write("What is your grade?"); 
+         string grade_student = Console.ReadLine(); 
+         int grade = int.Parse(grade_student);
+
+        if (grade >= 90)
+        {
+            Console.Write("Your grade is A");
+        }
+        
+        else if (grade >= 80) 
+        {
+            Console.Write("Your grade is B");
+        }
+
+        else if (grade >= 70) 
+        {
+            Console.Write("Your grade is C");
+        }
+
+        else if (grade >= 60) 
+        {
+            Console.Write("Your grade is D");
+
+        }
+
+        else 
+        {
+            Console.Write("Your grade is F");
+        }
+
+
+        
+
     }
 }
